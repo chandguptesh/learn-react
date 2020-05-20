734bb563a0408a7d0813e2beeb35a16183c51770
GupteshPC\Guptesh <guptesh.146@gmail.com>
2020-05-20 22:06:08 +0530

    Updates

---
+++ precache-manifest.c91f6d1f885b605860b8372fa3fb0f7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6977d0a64b6c0a736faea289e2e256a2",
+    "url": "/chandguptesh/learn-react/index.html"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/chandguptesh/learn-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8222a785240f15ffb2f6",
+    "url": "/chandguptesh/learn-react/static/css/main.f6d93501.chunk.css"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/chandguptesh/learn-react/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chandguptesh/learn-react/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8222a785240f15ffb2f6",
+    "url": "/chandguptesh/learn-react/static/js/main.077d8167.chunk.js"
+  },
+  {
+    "revision": "f24815527372ca8cdf71",
+    "url": "/chandguptesh/learn-react/static/js/runtime-main.e34ef831.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/chandguptesh/learn-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
